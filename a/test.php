e762d40b9e8317b28c7a4c70ce564a7bef7aa5f7
seyyah <seyyah@bil.omu.edu.tr>
2012-01-14 11:40:11 +0200

    fonksiyon örneği eklendi

--- a/test.php
+++ a/test.php
@@ -2,6 +2,10 @@
 
 require_once 'lib/base.php';
 
+function fun_exam($a, $b) {
+	return 1.0 * $a + 1.0 * $b;
+}
+
 // Set up
 F3::clear('TEST');
 F3::mock('GET /');
@@ -9,11 +13,14 @@ F3::mock('GET /');
 F3::call('index.php'); // Our test fixture will run in a sandbox
 
 echo "<hr>Test<hr>";
+
 // This is where the tests begin
-F3::expect(F3::get('RESPONSE'),'Passed','Failed: No response');
-F3::expect('{{is_string(@RESPONSE)}}','OK','Failed: Not a string');
-F3::expect('{{strlen(@RESPONSE)<120}}','Great!','Failed: Too long');
-F3::expect('{{!@ERROR}}','Done','Failed: An error occurred');
+F3::expect(F3::get('RESPONSE'),		'Passed',	'Failed: No response');
+F3::expect('{{is_string(@RESPONSE)}}',	'OK',		'Failed: Not a string');
+F3::expect('{{strlen(@RESPONSE)<120}}',	'Great!',	'Failed: Too long');
+F3::expect('{{!@ERROR}}',		'Done',		'Failed: An error occurred');
+F3::expect(fun_exam(1,2) == 3,		'OK: 1+2=>3',	'Failed: 1+2');
+F3::expect(fun_exam("1","2") == '12',	'OK: "1"+"2"=>"3"', 'Failed: "1"+"2"');
 
 // Display the results of each test
 $passed = 0; $failed = 0;
@@ -26,12 +33,6 @@ foreach (F3::get('TEST') as $test) {
 		$failed++;
 }
 echo '<br/>';
-
-// If you have no need for detailed messages, you can do this:
-// rotate TEST array variable so rows become columns, and vice-versa
-F3::set('TEST',Matrix::transpose(F3::get('TEST')));
-// then, distribute the number of passed/failed tests
-//list($passed,$failed)=array_count_values(F3::get('TEST.result'));
 
 if ($failed) echo 'Our test fixture failed in '.$failed.' tests.';
 else echo 'Hoohah!';
